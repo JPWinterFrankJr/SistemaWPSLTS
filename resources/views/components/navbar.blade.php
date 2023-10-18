ce9146a3decde14fcf811543547aab928f4884de
João Pedro <joaopedrowinter10@gmail.com>
2023-10-18 19:19:16 -0300

    Criando Views

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -11,6 +11,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
     </script>
+    @stack('css')
 </head>
 
 <body>
@@ -41,6 +42,9 @@
             </div>
         </div>
     </nav>
+
+    @yield('content')
+
 </body>
 
 </html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('components.navbar')
+
+@section('content')
+    <div>
+        <h1 class="text-center text-success mt-5">Bem vindo</h1>
+        <p class="text-center">Este é o sistema desenvolvido para Winter Prestadora de Serviços lmts onde a mesma poderá 
+            cadastrar e visualizar seus clientes, maquinarios e entre outras funções</p>
+    </div>
+@endsection
